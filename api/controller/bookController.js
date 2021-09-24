75505e1fe1f82c99d19754c7eb2660debfc19f9a
lopes-joe <slopes.joe@gmail.com>
2021-09-24 11:32:56 -0300

    added verbs: get-by-id, post, put, delete

--- api/controller/bookController.js
+++ api/controller/bookController.js
@@ -14,3 +14,45 @@ exports.listAllBooks = (req, res)=>{
     })
 }
 
+//GET ID
+exports.listOneBooks = (req, res)=>{
+    book.find({"_id":req.params.bookId}, (error, book)=>{
+        if(error){
+            res.send(error)
+        } else {
+            res.json(book)
+        }
+    })
+}
+
+//POST
+exports.addOneBooks = (req, res)=>{
+    newBook = new book(req.body)
+    newBook.save((error, book)=>{
+        if(error){
+            res.send(error)
+        }
+        res.json(book)
+    })
+}
+
+//PUT
+exports.updateBook = (req, res)=>{
+    book.findOneAndUpdate({"_id":req.params.bookId}, req.body, {new: true}, (error, book)=>{
+        if(error){
+            res.send(error)
+        }
+        res.json(book)
+    })
+}
+
+//DELETE
+exports.removeBook = (req, res)=>{
+    book.remove({_id: req.params.bookId}, (error, book)=>{
+        if(error){
+            res.send(error)
+        } else {
+            res.json({message: "your book was deleted"})
+        }
+    })
+}
